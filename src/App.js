8a75b12815991a26d3510190491511095ee16c57
cky1312 <cky13129@gmail.com>
2021-03-28 16:43:13 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="App-input">
+          <p>Enter Comma Separated Roll Number</p>
+          <input type="text" placeholder={"Input Comma Separated Roll Numbers"}/>
+          <br/>
+          <br/>
+          <button>
+              Submit
+          </button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
